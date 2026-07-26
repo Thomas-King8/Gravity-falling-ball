dcaaa519192dcf3b63ba5e10fbe837ffb6705ec8
Thomas-King8 <thomasdanielking@icloud.com>
2026-07-26 19:15:41 -0400

    added rocket thrust

--- main.py
+++ main.py
@@ -3,56 +3,69 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
 gravity = -9.81
-mass = 10
 radius = 0.01
 A = np.pi * radius ** 2
 rho = 1.225
 Cd = 0.47
 dt = 0.01
+thrust = 500
+burn_time = 10 
+fuel_mass = 5
+dry_mass = 5
 
-initialSpeed = 50
+
+initialSpeed = 0
 initialAngle = np.radians(45)
 
 
 
 
-ball_x = 0
-ball_vx = initialSpeed * np.cos(initialAngle)
-ball_y = 0
-ball_vy = initialSpeed * np.sin(initialAngle)
+rocket_x = 0
+rocket_vx = initialSpeed * np.cos(initialAngle)
+rocket_y = 0
+rocket_vy = initialSpeed * np.sin(initialAngle)
 
 x_positions = []
 y_positions = []
 
 
-for step in range(6000):
-    speed = np.sqrt(ball_vx ** 2 + ball_vy ** 2)
-    drag_y = -0.5 * rho * speed * ball_vy * Cd * A
-    drag_x = -0.5 * rho * speed * ball_vx * Cd * A
+for step in range(100000):
+    time = step * dt 
+    if time < burn_time:
+        current_mass = dry_mass + fuel_mass * (1 - time/burn_time)
+        thrust_force = thrust 
+    else:
+        current_mass = dry_mass
+        thrust_force = 0 
+    
+    thrust_x = thrust_force * np.cos(initialAngle)
+    thrust_y = thrust_force * np.sin(initialAngle)
+    
+    speed = np.sqrt(rocket_vx ** 2 + rocket_vy ** 2)
+    drag_y = -0.5 * rho * speed * rocket_vy * Cd * A
+    drag_x = -0.5 * rho * speed * rocket_vx * Cd * A
     
     # Y 
     
-    force_gravity = mass * gravity 
+    force_gravity = current_mass * gravity 
     
-    force = force_gravity + drag_y
-    ay = force/mass
+    force = force_gravity + drag_y + thrust_y
+    ay = force/current_mass
     
-    ball_vy = ball_vy + ay * dt
-    ball_y = ball_y + ball_vy * dt
+    rocket_vy = rocket_vy + ay * dt
+    rocket_y = rocket_y + rocket_vy * dt
     
-    if ball_y <= 0:
-        ball_y = 0 
-        ball_vy = -ball_vy * 0.9
-
+    if rocket_y <= 0 and step > 0:
+        break
     # X
     
-    force = drag_x
-    ax = force/mass
-    ball_vx = ball_vx + ax * dt
-    ball_x = ball_x + ball_vx * dt
+    force = drag_x + thrust_x
+    ax = force/current_mass
+    rocket_vx = rocket_vx + ax * dt
+    rocket_x = rocket_x + rocket_vx * dt
     
-    y_positions.append(ball_y)
-    x_positions.append(ball_x)
+    y_positions.append(rocket_y)
+    x_positions.append(rocket_x)
     
 plt.plot(x_positions, y_positions)
 plt.xlabel('Distance (m)')
